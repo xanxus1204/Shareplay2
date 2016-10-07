05f61cf0cdd1016d447f5799e622ab17711583f8
suguru <icloud_i4s.com>
2016-10-07 16:13:33 +0900

    家でやります

---
+++ SharePlay2/AudioQueuePlayer.swift
@@ -0,0 +1,12 @@
+//
+//  AudioQueuePlayer.swift
+//  SharePlay2
+//
+//  Created by 椛島優 on 2016/10/07.
+//  Copyright © 2016年 椛島優. All rights reserved.
+//
+
+import Foundation
+class AudioQueuePlayer: NSObject {
+    
+}
